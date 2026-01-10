37400ca0e112606459138f8ac2b1fa40e17c8345
V0yager9 <151874463+V0yager9@users.noreply.github.com>
2026-01-10 15:16:42 +0100

    Struttura classe Tutor

---
+++ include/tutor.h
@@ -0,0 +1,23 @@
+// Autore: Davide Cossalter
+
+#ifndef TUTOR_H
+#define TUTOR_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+// Sistema di controllo dei transiti dei veicoli
+class Tutor 
+{
+	public:
+		Tutor();
+
+
+	private:
+		
+};
+
+#endif
+
